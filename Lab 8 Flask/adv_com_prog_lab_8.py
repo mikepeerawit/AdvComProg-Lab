7d219f865a4eb2625e6a08bf99c3cd929d1fd766
Peerawit Chariyawongsiri <michael.chry@gmail.com>
2020-10-24 21:46:11 +0700

    Update adv_com_prog_lab_8.py

--- Lab 8 Flask/adv_com_prog_lab_8.py
+++ Lab 8 Flask/adv_com_prog_lab_8.py
@@ -32,7 +32,7 @@ def home():
 
 @app.route('/insertscore')
 def insertscore():
-  client=MongoClient("mongodb+srv://usertest:usertest@cluster0.ob578.mongodb.net/retryWrites=true&w=majority")
+  client=MongoClient("mongodb+srv://usertest:<password>@cluster0.ob578.mongodb.net/<db-name>retryWrites=true&w=majority")
   db=client.all_score
   for value in all_scores:
     db.score.insert_one(value)
@@ -66,7 +66,7 @@ def home():
 
 @app.route('/insertscore')
 def insertscore():
-  client=MongoClient("mongodb+srv://usertest:usertest@cluster0.ob578.mongodb.net/retryWrites=true&w=majority")
+  client=MongoClient("mongodb+srv://usertest:<password>@cluster0.ob578.mongodb.net/<db-name>retryWrites=true&w=majority")
   db=client.all_score
   for value in all_scores:
     db.score.insert_one(value)
